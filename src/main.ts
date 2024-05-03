84e53264a85a6cb17b34f3bf81cbc6a5ad9f19db
AlejandroXVII <m.alejandro.xvii@gmail.com>
2024-05-02 23:39:32 -0400

    Implement reload button and input to control size

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import "./style.css";
 import grass_SRC from "./textures/grass.png";
+import tiny_grass_SRC from "./textures/tiny-grass.png";
+import flower_SRC from "./textures/flower.png";
+import tiny_flower_SRC from "./textures/tiny-flower.png";
 import sand_SRC from "./textures/sand.png";
 import water_SRC from "./textures/water.png";
 import c_sbl_w_SRC from "./textures/c-sbl-w.png";
@@ -137,9 +140,9 @@ type weightType = {
 	[key: string]: number;
 };
 const weight: weightType = {
-	water: 5,
+	water: 10,
 	grass: 30,
-	sand: 5,
+	sand: 20,
 	"c-sbl-w": 1,
 	"c-slb-w": 1,
 	"c-stl-w": 1,
@@ -195,7 +198,7 @@ let columns = 30;
 let rows = 30;
 let matrix = new Map<string, CellType>();
 let entropyCellList: CellType[] = [];
-let counter = 0;
+let counter: number;
 // Function that choose a value of the entropy values of a Cell
 //An then propagate to the next to cell
 /** @param {number[]} coordinate - The coordinate of the obj in the matrix that its going to collapse*/
@@ -228,12 +231,8 @@ function collapse(coordinate: number[]): void {
 
 	if (!isCellType(cell)) return;
 
-	//let entropyIndex: number;
-	//entropyIndex = getRandom(cell.entropyValues.length);
-
 	cell.isCollapsed = true;
 	cell.value = getRandomByWeight(cell.entropyValues);
-	if (cell.value === "c-wbl-s") console.log(cell.entropyValues);
 	cell.entropyValues = [cell.value];
 
 	//Get the up, down, left and right keys to the cells value object
@@ -264,100 +263,140 @@ function collapse(coordinate: number[]): void {
 			collapse(newCurrentCell.coordinate);
 		}
 	} else {
-		console.log(entropyCellList);
 		renderTerrane();
 	}
 }
 
 function renderTerrane() {
-	window.onload = () => {
-		const container = document.getElementById("terrane-container");
-		console.log(container);
-		for (let x = 0; x < columns; x++) {
-			for (let y = 0; y < rows; y++) {
-				const textureIMG = document.createElement("img");
-				const currentCell = matrix.get([x, y].toString());
-				if (!isCellType(currentCell)) return;
-				if (currentCell.entropyValues.length === 0)
-					currentCell.entropyValues.push(currentCell.value);
-				currentCell.value = currentCell.entropyValues[0];
-				switch (currentCell.value) {
-					case "water":
-						textureIMG.src = water_SRC;
-						break;
-					case "grass":
-						textureIMG.src = grass_SRC;
-						break;
-					case "sand":
-						textureIMG.src = sand_SRC;
-						break;
-					case "c-slb-w":
-						textureIMG.src = c_slb_w_SRC;
-						break;
-					case "c-stl-w":
-						textureIMG.src = c_stl_w_SRC;
-						break;
-					case "c-str-w":
-						textureIMG.src = c_str_w_SRC;
-						break;
-					case "c-sbl-w":
-						textureIMG.src = c_sbl_w_SRC;
-						break;
-					case "c-wbl-s":
-						textureIMG.src = c_wbl_s_SRC;
-						break;
-					case "c-wrb-s":
-						textureIMG.src = c_wrb_s_SRC;
-						break;
-					case "c-wtl-s":
-						textureIMG.src = c_wtl_s_SRC;
-						break;
-					case "c-wtr-s":
-						textureIMG.src = c_wtr_s_SRC;
-						break;
-					case "sr-wl":
-						textureIMG.src = sr_wl_SRC;
-						break;
-					case "st-wb":
-						textureIMG.src = st_wb_SRC;
-						break;
-					case "wr-sl":
-						textureIMG.src = wr_sl_SRC;
-						break;
-					case "wt-sb":
-						textureIMG.src = wt_sb_SRC;
-						break;
-					default:
-						break;
-				}
-				if (currentCell?.coordinate !== undefined)
-					textureIMG.id = currentCell?.coordinate.toString();
-				container?.appendChild(textureIMG);
-				container?.setAttribute(
-					"style",
-					"display: grid;grid-template-columns: repeat(" +
-						columns +
-						", auto);justify-content: center;"
-				);
+	const container = document.getElementById("terrane-container");
+	for (let x = 0; x < columns; x++) {
+		for (let y = 0; y < rows; y++) {
+			const textureIMG = document.createElement("img");
+			const currentCell = matrix.get([x, y].toString());
+			if (!isCellType(currentCell)) return;
+			if (currentCell.entropyValues.length === 0)
+				currentCell.entropyValues.push(currentCell.value);
+			currentCell.value = currentCell.entropyValues[0];
+			switch (currentCell.value) {
+				case "water":
+					textureIMG.src = water_SRC;
+					break;
+				case "grass":
+					switch (getRandom(4)) {
+						case 0:
+							textureIMG.src = grass_SRC;
+							break;
+						case 1:
+							textureIMG.src = tiny_grass_SRC;
+							break;
+						case 2:
+							textureIMG.src = flower_SRC;
+							break;
+						case 3:
+							textureIMG.src = tiny_flower_SRC;
+							break;
+						default:
+							break;
+					}
+					break;
+				case "sand":
+					textureIMG.src = sand_SRC;
+					break;
+				case "c-slb-w":
+					textureIMG.src = c_slb_w_SRC;
+					break;
+				case "c-stl-w":
+					textureIMG.src = c_stl_w_SRC;
+					break;
+				case "c-str-w":
+					textureIMG.src = c_str_w_SRC;
+					break;
+				case "c-sbl-w":
+					textureIMG.src = c_sbl_w_SRC;
+					break;
+				case "c-wbl-s":
+					textureIMG.src = c_wbl_s_SRC;
+					break;
+				case "c-wrb-s":
+					textureIMG.src = c_wrb_s_SRC;
+					break;
+				case "c-wtl-s":
+					textureIMG.src = c_wtl_s_SRC;
+					break;
+				case "c-wtr-s":
+					textureIMG.src = c_wtr_s_SRC;
+					break;
+				case "sr-wl":
+					textureIMG.src = sr_wl_SRC;
+					break;
+				case "st-wb":
+					textureIMG.src = st_wb_SRC;
+					break;
+				case "wr-sl":
+					textureIMG.src = wr_sl_SRC;
+					break;
+				case "wt-sb":
+					textureIMG.src = wt_sb_SRC;
+					break;
+				default:
+					break;
 			}
+			if (currentCell?.coordinate !== undefined)
+				textureIMG.id = currentCell?.coordinate.toString();
+			container?.appendChild(textureIMG);
+			container?.setAttribute(
+				"style",
+				"display: grid;grid-template-columns: repeat(" +
+					rows +
+					", auto);justify-content: center;"
+			);
 		}
-	};
-}
-
-//Initialized the matrix with all its values as CellType
-for (let indexColumn = 0; indexColumn < columns; indexColumn++) {
-	for (let indexRow = 0; indexRow < rows; indexRow++) {
-		const cell = new Cell([indexColumn, indexRow]);
-		matrix.set([indexColumn, indexRow].toString(), cell);
 	}
 }
 
+//Initialized the matrix with all its values as CellType
+function initialized() {
+	counter = 0;
+	entropyCellList = [];
+	for (let indexColumn = 0; indexColumn < columns; indexColumn++) {
+		for (let indexRow = 0; indexRow < rows; indexRow++) {
+			const cell = new Cell([indexColumn, indexRow]);
+			matrix.set([indexColumn, indexRow].toString(), cell);
+		}
+	}
+}
+initialized();
 let cellToCollapseCoordinate = [getRandom(columns), getRandom(rows)];
-console.log(cellToCollapseCoordinate);
 collapse(cellToCollapseCoordinate);
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
+	<div>
+		<h1>Generate procedural terrains!</h1>
+		<h2>There are <span>${columns * rows * 18}</span> diferent posibilities</h2>
+	</div>
+	<div>
+	<input id="columns" type="number" placeholder="Columns" />
+	<input  id="rows" type="number" placeholder="Rows" />
+	<button id="generator">Generate other</button>
+	</div>
     <div id="terrane-container"></div>
+	
   </div>
 `;
+let $terrainContainer = document.getElementById("terrane-container");
+let $buttonGenerator = document.getElementById("generator");
+let $columnsInput = <HTMLInputElement>document.getElementById("columns");
+let $rowsInput = <HTMLInputElement>document.getElementById("rows");
+if ($buttonGenerator !== null) {
+	$buttonGenerator.onclick = function () {
+		columns = Number($columnsInput?.value);
+		rows = Number($rowsInput?.value);
+		if ($terrainContainer !== null) {
+			$terrainContainer.innerHTML = "";
+			matrix.clear();
+			initialized();
+			collapse([1, 1]);
+		}
+	};
+}
